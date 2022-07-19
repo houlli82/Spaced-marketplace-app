52af43980b8eb3be903230dde23c0ea1b1dc8166
Jonathan Rudd <jonathanrudd3@gmail.com>
2022-07-19 20:51:10 +0900

    seeds added for users and spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "creating 5 users"
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+end
+
+puts "5 users created"
+
+puts "making 10 spaces"
+
+10.times do
+  Space.create!(
+    location: Faker::Address.city,
+    capacity: rand(2..50),
+    fee: rand(1..100) * 1000,
+    size: Faker::Number.number(digits: 2),
+    user_id: User.all.sample[:id]
+  )
+end
+
+puts "10 spaces created"
