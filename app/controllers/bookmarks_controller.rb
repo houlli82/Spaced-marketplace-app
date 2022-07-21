d5b94826aa249c15efdd1d5d5ac251e3b5a26cbe
January Amanda <january.amanda@gmail.com>
2022-07-21 19:34:21 +0900

    Bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.new(bookmark_params)
+    if @bookmark.save
+      redirect_to @bookmark.list
+    else
+      render :new
+    end
+
+    def destroy
+      @list = List.find(params[:list_id])
+      @bookmark = @list.bookmarks.destroy
+      redirect_to @bookmark.list
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:space_id, :list_id)
+  end
 end
