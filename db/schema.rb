0df4b83f034eac4b625087a29ed81feb27bca422
January Amanda <january.amanda@gmail.com>
2022-07-23 11:02:30 +0900

    Doing Index Bookings halfway done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_110253) do
+ActiveRecord::Schema.define(version: 2022_07_21_120018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2022_07_19_110253) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["user_id"], name: "index_spaces_on_user_id"
   end
 
