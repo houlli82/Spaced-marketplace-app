bdf6621d41d0b38974db8b68e338a3ed810c91d8
January Amanda <january.amanda@gmail.com>
2022-07-27 21:59:52 +0900

    Ugliest pgsearch bar but it works

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,13 @@
 class Space < ApplicationRecord
   belongs_to :user
-
   has_many_attached :photos
+
+  include PgSearch::Model
+    multisearchable against: [:title, :location]
+
+      pg_search_scope :search_by_title_and_location,
+        against: [ :title, :location ],
+        using: {
+          tsearch: { prefix: true }
+        }
 end
