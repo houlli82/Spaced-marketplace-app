e62ec66ba12b596f6e23fd110d5f77ee9efd40c3
January Amanda <january.amanda@gmail.com>
2022-07-30 12:05:06 +0900

    Add desc table and seeds add some html for space title

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -35,6 +35,6 @@ class SpacesController < ApplicationController
   private
 
   def space_params
-    params.require(:space).permit(:location, :capacity, :fee, :size, :photo)
+    params.require(:space).permit(:location, :capacity, :fee, :size, :photo, :description)
   end
 end
