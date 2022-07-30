2184790a1672d7b358e3f822a49ec500002f525d
mark_houlli <markhoulli@protonmail.com>
2022-07-30 15:22:37 +0900

    Added markers and pop up to markers

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -17,13 +17,22 @@ export default class extends Controller {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     this._addMarkersToMap();
+    this._fitMapToMarkers();
   }
 
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
         .addTo(this.map);
     });
+  }
+
+  _fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+    this.map.fitBounds(bounds, { padding: 50, maxZoom: 15, duration: 4000 })
   }
 }
